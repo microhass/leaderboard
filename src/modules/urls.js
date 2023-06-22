d490879164ca7eefb5a1c52c2debd3f30a26f018
hassanShakur <hass.shakurhassan@gmail.com>
2023-06-22 15:38:28 +0300

    Updated game ID

--- src/modules/urls.js
+++ src/modules/urls.js
@@ -1,4 +1,4 @@
-const gameId = '2uhV0HoWniIqM5f9fC8g';
+const gameId = '6EJ4v384D0KMWfgikijg';
 const baseURL = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/';
 
 const url = `${baseURL}games/${gameId}/scores/`;
